ba7be0f90c1b6e7b8dba838dbdf44831d4755731
Ivan Woo <iwoo94@gmail.com>
2021-04-26 11:33:25 -0700

    implemented function to get top 100 ip addresses by count

--- trackIPaddresses.js
+++ trackIPaddresses.js
@@ -10,3 +10,15 @@ const requestHandled = (ipAddress) => {
    ipAddressTracker[ipAddress]++;
  }
 };
+
+const getTop100 = () => {
+  //i: none
+  //o: list of 100 ip addresses
+
+  let ipAddresses = Object.keys(ipAddressTracker);
+  let sortedAddresses = ipAddresses.sort((a, b) => {
+    return b - a;
+  });
+  let top100addresses = sortedAddresses.slice(0, 100);
+  return top100addresses;
+ };
